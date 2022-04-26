7404c35806c36f65b5d5f2399f16e36a4a2ad21e
agermakovskia <90781883+agermakovskia@users.noreply.github.com>
2022-04-25 20:59:24 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 
 int main ( int argc,char** argv)
 {
-    void* p = null;
+    void* p = 0x00;
     printf("AIS Hello Word!\n");
     if (argc == 3)
         memset(p, 0x00, 0xF);
