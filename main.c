729a6663299d7e2b8abd38fa2c0db52ec9868571
agermakovskia <90781883+agermakovskia@users.noreply.github.com>
2022-04-25 20:58:24 -0400

    Update main.c

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int main ( int argc,char** argv)
 {
     void* p = null;
     printf("AIS Hello Word!\n");
-    if (argc == 3}
+    if (argc == 3)
         memset(p, 0x00, 0xF);
         
     return 1;
